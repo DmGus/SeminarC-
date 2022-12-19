86bd8e15c420ec01bd69ca3c4c609e808038c160
Dmitrii Gushchin <dgushchin@gmail.com>
2022-12-19 15:05:49 +0300

    Add Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет. Выполнить с помощью числовых операций (целочисленное деление, 
+// остаток от деления).
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число и нажмите Enter: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ThirdDigit(num) == -1 ? $"{num} -> третьей цифры нет" : $"{num} -> {ThirdDigit(MakeThreeDigits(num))}");
+
+int ThirdDigit(int number)
+{
+    return number > 99 ? (number % 10) : -1;
+}
+
+int MakeThreeDigits(int numb)
+{
+    while (numb>1000) numb=numb/10;
+    return numb;
+}
