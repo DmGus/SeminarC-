add48a356d768fc69810d7b71588d2097b965def
Dmitrii Gushchin <dgushchin@gmail.com>
2022-12-19 11:45:33 +0300

    Add Task11

---
+++ Task12/Program.cs
@@ -0,0 +1,22 @@
+﻿// 12. Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли первое число
+// кратным второму. Если число 1 не кратно числу 2, то
+// программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трехзначное число => {number}");
+
+int maxDigit = MaxDigit(number);
+Console.Write($"Наибольшая цифра числа = {maxDigit}");
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return result;
+}
+
