02e8796abe55fae18737dd68177b066c396b8ae7
Dmitrii Gushchin <dgushchin@gmail.com>
2022-12-19 15:28:44 +0300

    Add Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите цифру, обозначающую день недели, и нажмите Enter: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine((num > 0 && num < 8) ? (Weekend(num) ? $"{num} -> да" : $"{num} -> нет") : "не правильный ввод");
+
+// Console.WriteLine(WeekDay(num) ? $"{num} -> да" : $"{num} -> нет");
+// if (num > 0 && num < 8) Console.WriteLine(WeekDay(num) ? $"{num} -> да" : "{num} -> нет");
+// else Console.WriteLine("не правильный ввод");
+
+bool Weekend(int number)
+{
+    return num == 6 || num == 7 ? true : false;
+}
