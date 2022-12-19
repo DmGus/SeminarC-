5a21c2d13c5176df97dae31dc90448764e65a9da
Dmitrii Gushchin <dgushchin@gmail.com>
2022-12-19 11:16:49 +0300

    Add Task09 Method Random

---
+++ Task09/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из диапазона 10 - 99 => {number}");
+
+int maxDigit = MaxDigit(number);
+Console.Write($"Наибольшая цифра числа = {maxDigit}");
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return result;
+}
